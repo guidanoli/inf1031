06808aeaabe83c9ac4a11f44f17b555d28b97246
GUILHERME DANTAS DE OLIVEIRA <e1713155@DANUBIO.LAB-GRAD>
2018-09-17 12:58:06 -0300

    Comecei VERTICE e ANALIZADOR LÉXICO

---
+++ SOURCE/ALEX.C
@@ -0,0 +1,35 @@
+/***************************************************************************
+*  $MCI Módulo de implementação: ALX Analizador Léxico
+*
+*  Arquivo gerado:              ALEX.c
+*  Letras identificadoras:      ALX
+*
+*  Curso: INF 1301 Programação Modular
+*  Orientador: Flavio Bevilacqua
+*  Autores:   gui   Guilherme Dantas
+*             cai   Caique Molina
+*             nag   Nagib Suaid
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*     1       gui   17/09/2018  início do desenvolvimento
+*
+***************************************************************************/
+
+#include   <stdio.h>
+#include   <string.h>
+#include   <memory.h>
+#include   <malloc.h>
+#include   <assert.h>
+
+#define ALEX_OWN
+#include "ALEX.h"
+#undef ALEX_OWN
+
+/***** Protótipos das funções encapuladas no módulo *****/
+
+/*****  Código das funções exportadas pelo módulo  *****/
+
+/*****  Código das funções encapsuladas no módulo  *****/
+
+/********** Fim do módulo de implementação: ALX  Analizador Léxico **********/
---
+++ SOURCE/VERTICE.C
@@ -0,0 +1,60 @@
+/***************************************************************************
+*  $MCI Módulo de implementação: VER Vértice
+*
+*  Arquivo gerado:              VERTICE.c
+*  Letras identificadoras:      VER
+*
+*  Curso: INF 1301 Programação Modular
+*  Orientador: Flavio Bevilacqua
+*  Autores:   gui   Guilherme Dantas
+*             cai   Caique Molina
+*             nag   Nagib Suaid
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*     1       gui   17/09/2018  início do desenvolvimento
+*
+***************************************************************************/
+
+#include   <stdio.h>
+#include   <string.h>
+#include   <memory.h>
+#include   <malloc.h>
+#include   <assert.h>
+
+#define VERTICE_OWN
+#include "VERTICE.h"
+#undef VERTICE_OWN
+
+/***********************************************************************
+*
+*  $TC Tipo de dados: VER Vértice
+*
+*
+***********************************************************************/
+
+   typedef struct VER_tagVertice {
+
+          void * Valor ;
+               /* Ponteiro genérico para o valor armazenado no Vértice */
+
+          void ( * ExcluirValor ) ( void * pDado ) ;
+               /* Ponteiro para a função de destruição do valor contido em um elemento */
+
+   } VER_tpVertice ;
+
+/***** Protótipos das funções encapuladas no módulo *****/
+
+/*****  Código das funções exportadas pelo módulo  *****/
+
+/***************************************************************************
+*
+*  Função: VER  &Criar vértice
+*  ****/
+
+   // a fazer ainda
+
+
+/*****  Código das funções encapsuladas no módulo  *****/
+
+/********** Fim do módulo de implementação: VER  Vértice **********/
