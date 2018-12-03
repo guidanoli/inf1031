5b4e2af1d46607bd172ef4b886dd59ad0a3878c8
guidanoli <guidanoli@hotmail.com>
2018-12-03 15:46:22 -0200

    LABGRAD vai fechar... save

--- DISTRIBUICAO FINAL/TestaTudo.bat
+++ DISTRIBUICAO FINAL/TestaTudo.bat
@@ -1,3 +1,5 @@
-TRAB4-1.exe /s"TesteVerificaLista.script" >SaidaTesteListaComDeturp.log
 TRAB4-1.exe /s"TesteVerificaListaSemDeturpacoes.script" >SaidaTesteListaSemDeturp.log
+TRAB4-1.exe /s"TesteVerificaLista.script" >SaidaTesteListaComDeturp.log
 TRAB4-2.exe /s"TesteLista.script" >SaidaTesteListaRelease.log
+start notepad "ContadoresListaComDeturp.count"
+start notepad "ContadoresListaSemDeturp.count"
