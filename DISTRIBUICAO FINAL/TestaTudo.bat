3db99e61f61c495775cb117c3181e577490e79ca
guidanoli <guidanoli@hotmail.com>
2018-12-03 15:14:18 -0200

    Distribuição final

---
+++ DISTRIBUICAO FINAL/TestaTudo.bat
@@ -0,0 +1,3 @@
+TRAB4-1.exe /s"TesteVerificaLista.script" >SaidaTesteListaComDeturp.log
+TRAB4-1.exe /s"TesteVerificaListaSemDeturpacoes.script" >SaidaTesteListaSemDeturp.log
+TRAB4-2.exe /s"TesteLista.script" >SaidaTesteListaRelease.log
